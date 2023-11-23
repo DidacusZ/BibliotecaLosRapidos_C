73de583739e6f6b1d71c75c54ab7d63afa6e1936
nocea <mnoceacabrera@gmail.com>
2023-11-23 14:02:44 +0100

    solo vistas

--- Poyecto-Gestor-Biblioteca-Web-Los-Rapidos/Program.cs
+++ Poyecto-Gestor-Biblioteca-Web-Los-Rapidos/Program.cs
@@ -1,22 +1,11 @@
-using DAL;
-using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<GestorBibliotecaDbContext>(
-     o => o.UseNpgsql(builder.Configuration.GetConnectionString("CadenaConexionPostgreSQL")));
-
 var app = builder.Build();
-/*
-using (var scope = app.Services.CreateScope())
-{
-    var appDBContext = scope.ServiceProvider.GetRequiredService<GestorBibliotecaDbContext>();
-    appDBContext.Database.Migrate();
-}
-*/
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
